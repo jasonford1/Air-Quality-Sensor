e20a22572201ddb0a742e7484b99bedf99b7db1c
jasonford1 <jasonford1@gmail.com>
2020-10-25 15:13:25 +0800

    Release candidate

--- particle.js
+++ particle.js
@@ -1,14 +1,14 @@
 //require('dotenv').config();
 const express = require('express');
-const tedious = require('tedious');
-const bodyParser = require('body-parser');
-const path = require('path');
+const { Connection, Request } = require('tedious');
+const TYPES = require('tedious').TYPES;
 
-// Create app
+// Create app and include urlencoded json parsing
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
 
 // Database configuration
-const { Connection, Request } = require('tedious');
 const db_config = require('./config/database');
 
 // Connect database
@@ -24,12 +24,13 @@ app.get('/particle', (req, res) => res.send('HELLO PARTICLE!'));
 // Respond to POST request at '/particle'
 app.post('/particle', (req, res) => {
     let data = JSON.parse(req.body.data);
+    console.log(data);
     // Convert air quality reading from string to INT for database
     let air_quality = (data['air-quality'] === 'Fresh Air') ? 0 : 
         (data['air-quality'] === 'Low Pollution') ? 1 :
         (data['air-quality'] === 'High Pollution') ? 2 :
         (data['air-quality'] === 'Danger') ? 3 : null;
-    // Build SQL request to insert sensor readings into database
+    // SQL request to insert sensor readings into database
     let request = new Request ('INSERT INTO [env-vals] (coreid, datetime, temp, humidity, pressure, air_quality, dust_lpo, dust_ratio, dust_concentration) VALUES (@coreid, @datetime, @temp, @humidity, @pressure, @air_quality, @dust_lpo, @dust_ratio, @dust_concentration)',
         (err, result) => {
         if(err) { console.error(err.message) }
@@ -41,7 +42,7 @@ app.post('/particle', (req, res) => {
     request.addParameter('temp', TYPES.SmallInt, data.temp)
     request.addParameter('humidity', TYPES.TinyInt, data.humidity)
     request.addParameter('pressure', TYPES.SmallInt, data.pressure)
-    request.addParameter('air_quality', TYPES.TinyInt, air_quality_val)
+    request.addParameter('air_quality', TYPES.TinyInt, air_quality)
     request.addParameter('dust_lpo', TYPES.Int, data['dust-lpo'])
     request.addParameter('dust_ratio', TYPES.Decimal, data['dust-ratio'], {precision:15,scale:7})
     request.addParameter('dust_concentration', TYPES.Decimal, data['dust-concentration'], {precision:15,scale:7})
@@ -52,5 +53,5 @@ app.post('/particle', (req, res) => {
 // Set connection port
 const PORT = process.env.PARTICLE_PORT || 3001;
 
-// Begin listening for requests and console.log upon success
+// Start server and console.log upon success
 app.listen(PORT, () => {console.log(`Server started on port ${PORT}`)} );
